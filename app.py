fbbde132a5ebafafbb6461603289625f23b89a08
Matthew Bailey <matthewmbja@gmail.com>
2023-09-23 06:46:27 -0500

    Updated the Api, file structure changed

--- app.py
+++ app.py
@@ -1,51 +1,21 @@
-from flask import Flask, request
-from db import items, stores
-import uuid
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+app.config["PROPAGATE_EXCEPTIONS"]= True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https:/cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+api = Api(app)
 
-@app.post('/store')
-def create_stores():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    new_store = {**store_data, "id": store_id}
-    stores[store_id] = new_store
-    return new_store, 201
-
-
-@app.post('/item')
-def create_item(name):
-    item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
-    
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item, 201
-
-
-@app.get("/store/<string:name>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        return {"message": "Store not found"}, 404
-
-
-@app.get("/item/<string:item_id>")
-def get_item(name):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"message": "Store not found"}, 404
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
